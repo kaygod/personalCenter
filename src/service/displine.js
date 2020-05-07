77bc968a449e68824634a88f0f83579b091847eb
wangkai <184641801@qq.com>
2020-05-07 15:29:31 +0800

    解决传空的内容

--- src/service/displine.js
+++ src/service/displine.js
@@ -32,8 +32,8 @@ exports.queryDays = async (user_id) => {
  * 查询当月的日期(要获取前后三个月的数据集合)
  */
 exports.currentMonthDays = async (user_id, date) => {
-  const startDate = moment(date).subtract(1,'months').format("YYYY-MM-DD");//获取上一个月的日期
-  const endDate = moment(date).add(2,'months').format("YYYY-MM-DD");//获取下两个月的日期
+  const startDate = moment(date).subtract(1, 'months').format('YYYY-MM-DD'); //获取上一个月的日期
+  const endDate = moment(date).add(2, 'months').format('YYYY-MM-DD'); //获取下两个月的日期
 
   const result = await Record.findAll({
     attributes: ['date'],
@@ -70,7 +70,27 @@ exports.getRecord = async (user_id, date) => {
   });
 
   if (result.length == 0) {
-    return null;
+    const sql_sentence =
+      'SELECT a.* FROM records AS a where a.user_id = :user_id AND datediff (a.date,:date) = 0';
+    const data = await seq.query(sql_sentence, {
+      replacements: {
+        user_id,
+        date,
+      },
+      type: Sequelize.QueryTypes.SELECT,
+    });
+    if (data.length == 0) {
+      return null;
+    } else {
+      const { record_id, date, is_record, declaration } = data[0];
+      return {
+        declaration,
+        record_id,
+        date,
+        is_record,
+        tasks: [],
+      };
+    }
   } else {
     const tasks = [];
     const { record_id, date, is_record, declaration } = result[0];
